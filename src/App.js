4b9ba6f991715555bf0b269d53492a941f43fcc3
davidalantodd <david.todd@multiverse.io>
2023-06-01 20:31:12 -0400

    add starter code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Photo } from './components';
+
+function App() {
+  
+  
+    return (
+      <div className="container p-5 d-flex flex-column align-items-center">
+        <h1 className="mx-auto">{"Hello World"}</h1>
+        <Photo />
+      </div>
+    );
+  
+}
+
+export { App };
---
+++ src/components/Photo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Photo() {
+  
+    return <img width="350" height="350" />;
+
+}
+
+export { Photo };
